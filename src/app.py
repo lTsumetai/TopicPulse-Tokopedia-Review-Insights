59042979d2d18f631340837f6bd1bf96be67cb36
Derida21 <derimhian@gmail.com>
2026-06-28 18:29:07 +0700

    'db_connection'

--- src/app.py
+++ src/app.py
@@ -8,12 +8,15 @@ st.set_page_config(
 
 page = st.sidebar.selectbox(
     'Select Page',
-    ['EDA', 'Prediction']
+    ['EDA', 'User','Prediction']
 )
 
 if page == 'EDA':
     import eda
     eda.run()
+elif page == 'User':
+    import utils.testInput as testInput
+    testInput.run()
 else:
     import prediction
     prediction.run()
